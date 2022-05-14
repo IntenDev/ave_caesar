7ddae6c0b944978ebc113e6f2c99f283b8f2af23
ilakosterin <ilya@in10.top>
2022-05-14 17:54:08 +0300

    Закончил программу

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
-from icecream import ic
-dev ave_caecar():
-	pass
-	
-print(ave_caesar(i))
+eng = 'abcdefghijklmnopqrstuvwxyz'
+phrase_in = input()
+phrase = phrase_in.split()
+phrase_res = []
+len_eng = 26
+result = ''
+step = 0
+
+def ave_caesar(result):
+    for z in range(len(phrase)):
+        temp_result = ''
+        for y in range(len(phrase[z])):
+
+            if phrase[z][y].isalpha():
+                phrase_res.append(phrase[z][y])
+            elif y == 0 and not phrase[z][y].isalpha():
+                result = result + phrase[z][y]
+            else:
+                temp_result = phrase[z][y]
+
+        step = len(phrase_res)
+        temp = ''.join(phrase_res)
+
+        for i in range(len(temp)):
+            if ord('A') <= ord(temp[i]) <= ord('Z'):
+                result = result + eng[step - (len_eng - eng.find(temp[i].lower()))].upper()
+            else:
+                result = result + eng[step - (len_eng - eng.find(temp[i].lower()))]
+
+        result = result + temp_result
+        phrase_res.clear()
+        step = 0
+
+        print(result, end=' ')
+        result = ''
+
+
+ave_caesar(result)
+
+
